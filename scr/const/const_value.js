c0892696d8b2786536eaa4e590a4ce628bf0ced4
Hasan Masrur <mhbabu99@gmail.com>
2023-04-11 03:04:41 +0600

    added const_value

---
+++ scr/const/const_value.js
@@ -0,0 +1,4 @@
+const constValue ={
+    EMAIL_VALIDATION : /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+};
+export default constValue ;
--- scr/middlewares/validations/user.middleware.js
+++ scr/middlewares/validations/user.middleware.js
@@ -2,10 +2,13 @@
 import StatusCode from "../../const/status_code.js";
 import Status from "../../const/status.js"; 
 import ErrorCode from "../../const/error_code.js";
+import constValue from "../../const/const_value.js";
 import ResponseHelper from "../../helpers/response/response.helper.js"
 export const UserRegValidationMiddleware = async (req,res,next) =>{
     try {
-        const {name,username,email,password}= req.body;
+      
+     const {name,username,email,password}= req.body;
+console.log(password.length);
         if(!name)
      return   res.status(StatusCode.BAD_REQUEST).json(
             ResponseHelper.message(
@@ -39,6 +42,25 @@ export const UserRegValidationMiddleware = async (req,res,next) =>{
             )
             
         );
+        if(password.length <= 7)
+        return   res.status(StatusCode.BAD_REQUEST).json(
+               ResponseHelper.message(
+                   ErrorCode.VALIDATION_ERROR,
+                   Status.ERROR,
+                   "password should more then 6 character"
+               )   
+           );
+            if (! email.match(constValue.EMAIL_VALIDATION))  {
+                return   res.status(StatusCode.BAD_REQUEST).json(
+                    ResponseHelper.message(
+                        ErrorCode.VALIDATION_ERROR,
+                        Status.ERROR,
+                        "please provide a valid email"
+                    ) 
+                );
+           }
+         
+      
         next()
     } catch (error) {
         next(error);
