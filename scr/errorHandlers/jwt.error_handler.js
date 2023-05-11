0228523d753819554bf91b1c7ce0d167d3215339
Hasan Masrur <mhbabu99@gmail.com>
2023-05-11 15:51:58 +0600

    add jwt error handler

---
+++ scr/errorHandlers/jwt.error_handler.js
@@ -0,0 +1,24 @@
+import ErrorCode from "../const/error_code.js";
+import { ErrorMessage } from " . . /api/const/general. js";
+import StatusCode from "../const/status_code.js";
+import { AppError } from "./appError.js";
+
+const JwtErrorHandler ={
+    tokenExpiredError : (error)=>{
+        return new AppError(
+            "Token expired! please generate new token",
+            StatusCode. BAD_REQUEST,
+            ErrorCode. TOKEN_EXPIRED_ERROR
+        );
+    },
+    JsonWebTokenError: (error)=>{ 
+        return new AppError(
+             ErrorMessage.Auth,
+             StatusCode. UNAUTHORIZED,
+             ErrorCode. UNAUTHORIZED_ERROR
+             );
+            } 
+    };
+    export default JwtErrorHandler;
+
+
