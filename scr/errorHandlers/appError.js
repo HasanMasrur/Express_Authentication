fcd3f7d4a4d790c87f2b6fd34b70556ec86dceb8
Hasan Masrur <mhbabu99@gmail.com>
2023-05-11 15:36:30 +0600

    added app Error file

---
+++ scr/errorHandlers/appError.js
@@ -0,0 +1,11 @@
+import Status  from "../const/status";
+export class AppError extends Error{
+    constructor(message,statusCode,errorCode =null){
+        super(message);
+        this.statusCode = statusCode;
+        this.errorCode =errorCode;
+        this.status = `${statusCode}`.startsWith("4")?Status.FAIL : Status.ERROR;
+        this.isOperational = true ;
+        Error.captureStackTrace(this,this.constructor);
+    }
+}
