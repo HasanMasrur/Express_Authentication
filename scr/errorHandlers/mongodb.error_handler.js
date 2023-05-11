76878b965341955aa25cde2ea8069323a3433954
Hasan Masrur <mhbabu99@gmail.com>
2023-05-11 16:03:46 +0600

    added mongodb error handler

---
+++ scr/errorHandlers/mongodb.error_handler.js
@@ -0,0 +1,28 @@
+import ErrorCode from "../const/error_code.js";
+import StatusCode from "../const/status_code.js";
+import { AppError } from "./appError.js";
+
+const MongodbErrorHandler = {
+castError: (error) => {
+const message = `Invalid ${error.path}: ${error.value}`;
+return new AppError(
+message,
+StatusCode. BAD_REQUEST,
+ErrorCode.BAD_REQUEST_ERROR
+);
+},
+duplicateError: (error) => {
+const { message: errorMessage } = error;
+const searchstring = "index:";
+let startingindex= errorMessage. index0f (searchString);
+const endIndex = errorMessage. indexof (" _1");
+startingIndex = startingIndex + searchString. length;
+const value = errorMessage. substring(startingIndex, endIndex);
+const message = `Duplicate field value:${value}.Please use another value!`;
+return new AppError(
+            message,
+            StatusCode. BAD_REQUEST,
+            ErrorCode. DUPLICATE_ERROR
+);
+},
+};
